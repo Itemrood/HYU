335b09ef0b6adca4c6eef0784725da3021bdfdc8
Itemrood <74306678+Itemrood@users.noreply.github.com>
2021-12-09 10:39:26 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+﻿SIGN_KEY = 'oc3q7ingf978mx457fgk4587fg847' # ключ игры. Лучше не трогать.
+RUCAPTCHA_KEY = 'f9ecc51ab8a03d18b60e9537c7915a06' # Ключик с рукапчи. https://rucaptcha.com/
+NAME = 'Danisyay741' #Ник-нейм аккаунта, должен быть индивидуальным, т.е. два одинаковых ника в игре быть не должно.
+USER_ID = 33510253 #Тут ID из HttpCanary
+TOKEN = '$2a$06$PmgFesgHRAZrqBKblkdFrO'
+HUMAN_CAPTCHA_SOLVE = False # Ручная капча True - будешь вводить сам. False - Будет работать рукапча
+level = 'INFO' # Лучше не трогать
+# По всем попросам писать https://funpay.ru/users/1251831/ или же https://t.me/NIKITA_6_6
